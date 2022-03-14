a197bac9de7fd79a895bb208a5f8bd8285f7bc61
smsaidurrahmanrobin <smsaiyeed@gmail.com>
2022-03-14 16:20:11 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return '<h1><center>welcome, manytomany</center></h1>';
+    return '<h1><center>welcome to the, manytomany relation crud</center></h1>';
 });
